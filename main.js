460fa71ca7dd0d9aee7a23b599ef1cd13366ef4f
Matheus Vaz <matheusvaz.ux@gmail.com>
2022-12-16 09:41:01 -0300

    first commit: add main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// Os caracteres \u no início do código são caracteres de escape que usamos para sinalizar ao JavaScript de que estamos falando de códigos Unicode, e não de strings de texto usuais.
+
+const cifrao = "\u0024";
+const aMaiusculo = "\u0041";
+const check = "\u2705";
+const hiragana = "\u3041";
+
+console.log(cifrao);
+console.log(aMaiusculo);
+console.log(check);
+console.log(hiragana);
